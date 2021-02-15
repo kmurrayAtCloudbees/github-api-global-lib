9a25c3ce35062d59025e2c5422aff6420db1faac
Kenneth Murray <kmurray@cloudbees.com>
2021-02-15 13:08:49 -0800

    added the rest of the params

--- vars/triggerCdPipeline.groovy
+++ vars/triggerCdPipeline.groovy
@@ -1,5 +1,5 @@
 def call(Map config=[:]) {
     sh "echo ${config.name} is starting a CD pipeline"
-    cloudBeesFlowRunPipeline pipelineName:"${config.pipelineName}",configuration:"${config.configuraiton}", projectName:"${config.projectName}"
+    cloudBeesFlowRunPipeline pipelineName:"${config.pipelineName}",configuration:"${config.configuration}", projectName:"${config.projectName}"
 }
 
