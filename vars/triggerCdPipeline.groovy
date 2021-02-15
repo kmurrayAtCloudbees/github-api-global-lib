c1a2b881cbabc77aff539b18fe7b7d7e582acc5a
Kenneth Murray <kmurray@cloudbees.com>
2021-02-15 10:56:28 -0800

    fixed syntax error

--- vars/triggerCdPipeline.groovy
+++ vars/triggerCdPipeline.groovy
@@ -1,4 +1,4 @@
 def call(String pipelineName, String configuration, String projectName) {
-  sh "cloudBeesFlowRunPipeline pipelineName: "${pipelineName"}, configuration: "${configuration}", projectName: "${projectName}""
+  sh "cloudBeesFlowRunPipeline pipelineName: "${pipelineName}", configuration: "${configuration}", projectName: "${projectName}""
 }
 
