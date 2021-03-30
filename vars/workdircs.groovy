95153dfc48666a9adb88fec310b0d2cde54fe1c1
Kenneth Murray <kmurray@cloudbees.com>
2021-03-30 15:49:12 -0700

    changes to workdircs

--- vars/workdircs.groovy
+++ vars/workdircs.groovy
@@ -1,17 +1,7 @@
-//def call(String workdir) {
 def call(Map config=[:]) {
-// This build currently puts the image in the local buildah repo.
-// The image will be build to the nexus repo, when that is made available.
     echo "Path: $PWD"
     echo "workdir: ${config.workdir}"
-    dir(${config.workdir}) {
-//        sh """
-          sh  "echo ${config.workdir} is in the lib"
-//            sudo docker ps
-//            pwd
-//            cp -R ${config.workdir}/src/main/resources/certs ${config.workdir}/.
-//            sudo buildah bud .
-//        """
-    }
+    dir "${config.workdir}"
+    sh "echo ${config.workdir} is in the lib"
     sh "echo Docker, umm I mean BUILDAH, build complete!"
     }
