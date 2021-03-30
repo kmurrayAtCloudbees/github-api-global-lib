6486b963d11a7f16b311b67cfd8db89d108a3a2f
Kenneth Murray <kmurray@cloudbees.com>
2021-03-30 15:58:41 -0700

    changes to workdircs

--- vars/workdircs.groovy
+++ vars/workdircs.groovy
@@ -1,7 +1,14 @@
 def call(Map config=[:]) {
     echo "Path: $PWD"
     echo "workdir: ${config.workdir}"
-//    dir "${config.workdir}"
+    dir ("${config.workdir}") {
+          sh """
+//	     sudo docker ps
+             pwd
+             cp -R ${config.workdir}/src/main/resources/certs ${config.workdir}/.
+//             sudo buildah bud .
+	  """
+    }
     sh "echo ${config.workdir} is in the lib"
     sh "echo Docker, umm I mean BUILDAH, build complete!"
     }
